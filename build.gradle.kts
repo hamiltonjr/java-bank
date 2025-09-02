bed916dcde4c3fe6d5961c8e385f50c4c7c75e01
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2025-09-01 21:33:13 -0300

    Inclusão de README.md (parcial)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("java")
 }
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 group = "br.com.dio"
 version = "1.0-SNAPSHOT"
 
@@ -10,6 +16,8 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
