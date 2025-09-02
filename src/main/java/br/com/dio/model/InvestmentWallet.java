2ac2eeb4fe07f7ac6cedd7559d62430b5acc0dfc
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2025-09-01 23:28:17 -0300

    Implementação de classes referentes a investimentos no nosso banco (Investment e InvestmentWallet)

---
+++ src/main/java/br/com/dio/model/InvestmentWallet.java
@@ -0,0 +1,29 @@
+package br.com.dio.model;
+
+import lombok.Getter;
+import lombok.ToString;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+@ToString
+@Getter
+public class InvestmentWallet extends Wallet {
+    private final Investment investment;
+    private final AccountWallet account;
+
+    public InvestmentWallet(final Investment investment, final AccountWallet account, final long amount) {
+        super(BankService.INVESTMENT);
+        this.investment = investment;
+        this.account = account;
+        addMoney(account.reduceMoney(amount), "investment");
+    }
+
+    public void updateAmount(final long percent) {
+        var amount = getFunds() * percent / 100;
+        var history = new MoneyAudit(UUID.randomUUID(), "rendimentos", OffsetDateTime.now());
+        var money = Stream.generate(() -> new Money(history)).limit(amount).toList();
+        this.money.addAll(money);
+    }
+
+}
