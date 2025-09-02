419ea9e8ce14338bf5cb8a38e0d1f769c41cd83b
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2025-09-01 23:01:54 -0300

    Implementação de algumas operações do nosso modelo (pacote model)

---
+++ src/main/java/br/com/dio/model/MoneyAudit.java
@@ -0,0 +1,9 @@
+package br.com.dio.model;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public record MoneyAudit(
+        UUID transactionId,
+        String description,
+        OffsetDateTime createdAt) {}
