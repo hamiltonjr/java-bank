49799e0dcdefe6dafc2aec7b442f0a5899f408a1
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2025-09-01 22:07:52 -0300

    Classe Main com método main() e pacotes que definem a estrutura do projeto

---
+++ src/main/java/br/com/dio/Main.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final int EXIT_SUCCESS = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Olá... seja bem-vindo ao DIO Bank!");
+    }
+}
