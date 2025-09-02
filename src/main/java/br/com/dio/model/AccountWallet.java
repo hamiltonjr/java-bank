3dc3434da135724196e81493396420150984cc62
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2025-09-01 23:10:51 -0300

    Implementação da classe AccountWallet, que implementa a classe abstrata Wallet para o projeto

---
+++ src/main/java/br/com/dio/model/AccountWallet.java
@@ -0,0 +1,26 @@
+package br.com.dio.model;
+
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+public class AccountWallet extends Wallet {
+    private final List<String> pix;
+
+    public AccountWallet(final List<String> pix) {
+        super(BankService.ACCOUNT);
+        this.pix = pix;
+    }
+
+    public AccountWallet(final long amount, final List<String> pix) {
+        super(BankService.ACCOUNT);
+        this.pix = pix;
+        addMoney(amount, "Valor de criação da conta");
+    }
+
+    public void addMoney(final long amount, final String description) {
+        var money = generatedMoney(amount, description);
+        this.money.addAll(money);
+    }
+
+}
